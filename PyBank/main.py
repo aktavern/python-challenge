4e5ce25cd73f47aff7a1339e99b44bf2e02fcac5
Ashley Schenker <aktavern@gmail.com>
2019-05-31 19:05:12 -0500

    initial version of pybank

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+import csv 
+
+with open('budget_data.csv','r') as file:
+    budget_data = csv.reader(file,delimiter=',')
+    next(budget_data)
+    budget_data = list(budget_data)
+
+    # calculate total number of months included in dataset
+    months = sum(1 for row in budget_data)
+    print (months)
+    net_total = 0
+
+
+
+# calculate net total amount of profit/losses over entire period
+# calculate average of changes in profit/losses over entire period
+# calculate greatest increase in profits (date and amount) over entire period
+# greatest decrease in losses (date and amount) over entire period 
