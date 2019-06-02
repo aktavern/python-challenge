73264907badb85e98b4add6443b6d1af09a309a5
Ashley Schenker <aktavern@gmail.com>
2019-06-02 16:05:05 -0500

    version 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,14 +7,15 @@ with open('budget_data.csv','r') as file:
 
     # calculate total number of months included in dataset
     months = sum(1 for row in budget_data)
-    print (months)
-    net_total = []
+    print (f'Total Months: {months}')
 
     # calculate net total amount of profit/losses over entire period
     net_total = [int(row[1]) for row in budget_data]
-    print(f'${sum(net_total)}')
+    net_total_sum = sum(net_total)
+    print(f'Total: ${net_total_sum}')
 
-# calculate net total amount of profit/losses over entire period
-# calculate average of changes in profit/losses over entire period
-# calculate greatest increase in profits (date and amount) over entire period
-# greatest decrease in losses (date and amount) over entire period 
+    # calculate average of changes in profit/losses over entire period
+
+    # calculate greatest increase in profits (date and amount) over entire period
+    
+    # greatest decrease in losses (date and amount) over entire period 
