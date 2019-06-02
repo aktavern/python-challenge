800a5c95d460620b11979c61be7afde60e49dd5e
Ashley Schenker <aktavern@gmail.com>
2019-06-02 15:30:14 -0500

    version 2 pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,11 @@ with open('budget_data.csv','r') as file:
     # calculate total number of months included in dataset
     months = sum(1 for row in budget_data)
     print (months)
-    net_total = 0
+    net_total = []
 
-
+    # calculate net total amount of profit/losses over entire period
+    net_total = [int(row[1]) for row in budget_data]
+    print(f'${sum(net_total)}')
 
 # calculate net total amount of profit/losses over entire period
 # calculate average of changes in profit/losses over entire period
